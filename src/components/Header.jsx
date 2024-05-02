7d8001ede771b3e3deb1d5ec2e6f63f7cb99808f
Miraz <w3miraj@gmail.com>
2024-05-02 19:07:58 +0600

    demo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
                 <a href="#"><img src={logo} alt="" /></a>
             </div>
             <div className="lg:w-[70%]">
-                <ul className={`lg:flex lg:gap-x-10 font-dmsans text-[14px] font-bold text-[#767676] lg:static ${show == true ? "bg-[green] text-white w-full absolute top-[80px] left-0 duration-700 ease-in-ou": "w-full absolute top-[-200px] left-0 duration-700 ease-in-ou"}`}>
+                <ul className={`lg:flex lg:gap-x-10 justify-center font-dmsans text-[14px] font-bold text-[#767676] lg:static ${show == true ? "bg-[green] text-white w-full absolute top-[80px] left-0 duration-700 ease-in-ou": "w-full absolute top-[-200px] left-0 duration-700 ease-in-ou"}`}>
                     <li><a className='hover:text-[#262626]' href="#">Home</a></li>
                     <li><a className='hover:text-[#262626]' href="#">Shop</a></li>
                     <li><a className='hover:text-[#262626]' href="#">About</a></li>
