6ddc123b50d439e177f479f45d455b6c9f532ac9
Miraz <w3miraj@gmail.com>
2024-05-04 16:42:33 +0600

    demo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,17 +13,17 @@ const Header = () => {
   return (
     <section className='h-[80px] items-center bg-[#FFFFFF]'>
         <Container>
-        <Flex className={" lg:flex justify-between h-[80px] items-center "}>
+        <Flex className={" lg:flex justify-between h-[80px] items-center px-3"}>
             <div className="lg:w-[40%]">
                 <a href="#"><img src={logo} alt="" /></a>
             </div>
             <div className="lg:w-[60%]">
-                <ul className={`lg:flex lg:gap-x-10 text-center font-dmsans text-[14px] font-bold text-[#767676] lg:static ${show == true ? "bg-[green] text-white w-full absolute top-[60px] right-0 duration-700 ease-in-ou": "w-0 absolute top-[-200px] left-0 duration-700 ease-in-ou"}`}>
-                    <li><a className='hover:text-[#262626]' href="#">Home</a></li>
-                    <li><a className='hover:text-[#262626]' href="#">Shop</a></li>
-                    <li><a className='hover:text-[#262626]' href="#">About</a></li>
-                    <li><a className='hover:text-[#262626]' href="#">Contacts</a></li>
-                    <li><a className='hover:text-[#262626]' href="#">Journal</a></li>
+                <ul className={`lg:flex lg:gap-x-10 py-4 font-dmsans text-[14px] font-bold text-[#767676] lg:static ${show == true ? "bg-[green] text-white  w-[200px] pl-[5%] absolute top-[60px] left-0 duration-700 ease-in-out z-[1]": " w-[200px] absolute top-[-200px] left-[-200px] duration-700 ease-in-out"}`}>
+                    <li className='py-2'><a className='hover:text-[#262626]' href="#">Home</a></li>
+                    <li className='py-2'><a className='hover:text-[#262626]' href="#">Shop</a></li>
+                    <li className='py-2'><a className='hover:text-[#262626]' href="#">About</a></li>
+                    <li className='py-2'><a className='hover:text-[#262626]' href="#">Contacts</a></li>
+                    <li className='py-2'><a className='hover:text-[#262626]' href="#">Journal</a></li>
                 </ul>
             </div>
             <div className="lg:hidden text-[30px]" onClick={()=> setShow (!show) }>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,41 +47,41 @@ const Navbar = () => {
 
 
   return (
-    <section className='bg-[#F5F5F3]'>
-        <Container>
-        <Flex className="h-[100px] items-center">
-            <div className="w-[30%] relative cursor-pointer" >
+    <section className='bg-[#F5F5F3] py-4 lg:py-0'>
+    <Container>
+        <div className="h-[100px] items-center flex flex-wrap px-3">
+            <div className=" lg:w-[30%] w-[45%] relative cursor-pointer order-1 lg:order-1" >
                 <div  ref={catref} className="flex items-center gap-x-2">
                     <HiOutlineBars3BottomLeft className='font-dmsans text-[24px] font-bold text-[#262626]'/>
                     <p className='font-dmsans text-[14px] font-bold text-[#262626]'>Shop by Category</p>
                 </div>
-                {catShow && <div className=" absolute top-[40px] left-0 w-[263px] bg-[#262626] rounded-[2px]">
+                {catShow && <div className=" absolute top-[40px] left-0 w-[263px] bg-[#262626] rounded-[2px] z-[1]">
                     <ul className=''>
-                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[31px] duration-300 ease-in-out border-b-2 border-[#2D2D2D]'>Accesories</li>
-                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[31px] duration-300 ease-in-out border-b-2 border-[#2D2D2D]'>Furniture</li>
-                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[31px] duration-300 ease-in-out border-b-2 border-[#2D2D2D]'>Electronics</li>
-                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[31px] duration-300 ease-in-out border-b-2 border-[#2D2D2D]'>Clothes</li>
-                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[31px] duration-300 ease-in-out border-b-2 border-[#2D2D2D]'>Bags</li>
-                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[31px] duration-300 ease-in-out'>Home appliances</li>
+                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[40px] duration-700 ease-in-out border-b-2 border-[#2D2D2D]'>Accesories</li>
+                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[40px] duration-700 ease-in-out border-b-2 border-[#2D2D2D]'>Furniture</li>
+                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[40px] duration-700 ease-in-out border-b-2 border-[#2D2D2D]'>Electronics</li>
+                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[40px] duration-700 ease-in-out border-b-2 border-[#2D2D2D]'>Clothes</li>
+                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[40px] duration-700 ease-in-out border-b-2 border-[#2D2D2D]'>Bags</li>
+                        <li className=' pl-[21px] py-[16px] font-dmsans text-[14px] font-normal text-[#ffffffab] hover:text-[#FFFFFF] hover:font-bold hover:pl-[40px] duration-700 ease-in-out'>Home appliances</li>
                     </ul>
                 </div>}
             </div>
 
-                <div className="w-[40%]">
+                <div className=" lg:w-[40%] order-3 lg:order-2">
                     <div className="flex items-center relative">
-                        <input type='search' placeholder='Search Products' className='h-[50px] w-[601px] outline-none px-5 font-dmsans text-[22px] font-bold text-[#262626] placeholder:text-[14px] placeholder:text-[#C4C4C4] placeholder:font-normal'/>
+                        <input type='search' placeholder='Search Products' className='h-[50px] lg:w-[601px] outline-none px-5 font-dmsans text-[22px] font-bold text-[#262626] placeholder:text-[14px] placeholder:text-[#C4C4C4] placeholder:font-normal justify-center'/>
                         <FaSearch className=' absolute top-[50%] right-8 translate-y-[-50%] text-[14px]'/>
                     </div>
                 </div>
 
-                <div className="w-[30%] flex relative justify-end gap-x-[30px]">
+                <div className=" lg:w-[30%] w-[55%] flex relative justify-end gap-x-[30px] order-2 lg:order-3">
                 <div className="">
                     <div className="flex" ref={accountref}>
                         <FaUser/>
                         <TiArrowSortedDown/>
                     </div>
 
-                    {accountShow && <div className=" absolute top-[25px] right-0">
+                    {accountShow && <div className="absolute top-[25px] right-0 z-[1] h-[100px] w-[203px]">
                         <div className="">
                                 <p className='py-[17px] px-[60px] font-dmsans text-[14px] font-bold text-[#262626] hover:bg-[#262626] hover:text-white'>My Account</p>
                         </div>
@@ -94,8 +94,8 @@ const Navbar = () => {
                 <div className=" "> 
 
             <div className=" cursor-pointer" ref={cartref}> <FaCartPlus/>  </div>
-                {cartShow && <div className="absolute top-[15px] right-0">
-                    <div className="p-5 bg-[#F5F5F3] flex">
+                {cartShow && <div className=" absolute top-[25px] right-0 z-[1] h-[241px] w-[360px]">
+                    <div className="w-full p-5 bg-[#F5F5F3] flex">
                         <div className="">
                             <img src={cartImg} alt="" />
                         </div>
@@ -109,7 +109,7 @@ const Navbar = () => {
                     </div>
                     
 
-                    <div className="pt-[14px] px-[22px] pb-[20px]">
+                    <div className="w-full pt-[14px] px-[22px] pb-[20px]">
                         <div className="">
                             <h4 className='font-dmsans text-[16px] font-normal leading-[23px] text-[#767676]'>Subtotal: <span className='font-dmsans text-[16px] font-bold leading-[23px] text-[#262626]'>$44.00</span></h4>
                         </div>
@@ -122,14 +122,13 @@ const Navbar = () => {
                                 <p className='py-4 px-10 border-2 border-[#2B2B2B] font-dmsans text-[14px] font-bold text-[#262626] hover:bg-[#262626] hover:text-white hover:border-transparent'>Checkout</p>
                             </div>
                         </div>
-
                     </div>
                 </div>}
                     
 
                 </div>
             </div>
-        </Flex>
+        </div>
     </Container>
     </section>
   )
